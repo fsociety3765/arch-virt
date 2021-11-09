58378b84d2bbf4a4fdf4eeab822f6998d37b6695
fsociety3765 <90276255+fsociety3765@users.noreply.github.com>
2021-11-09 21:18:37 +0000

    Update virt-setup.sh

--- virt-setup.sh
+++ virt-setup.sh
@@ -12,7 +12,7 @@ sudo reflector -a 48 -c ${ISO} -f 5 -l 20 --sort rate --save /etc/pacman.d/mirro
 sudo pacman -Syy
 
 echo "-------------------------------------------------"
-echo "Installing desktop environment packages          "
+echo "Installing packages                              "
 echo "-------------------------------------------------"
 PKGS=(
   'virt-manager'
@@ -28,3 +28,27 @@ for PKG in "${PKGS[@]}"; do
   echo "Installing: ${PKG}"
   paru -S "$PKG" --noconfirm --needed
 done
+
+echo "-------------------------------------------------"
+echo "Adding user (${USER}) to neccessary groups       "
+echo "-------------------------------------------------"
+sudo usermod -aG libvirt ${USER}
+sudo usermod -aG docker ${USER}
+
+echo "-------------------------------------------------"
+echo "Starting services                                "
+echo "-------------------------------------------------"
+sudo systemctl enable --now libvirtd
+sudo systemctl enable --now docker
+
+echo "-------------------------------------------------"
+echo "Installing Portainer & Watchtower                "
+echo "-------------------------------------------------"
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+sudo docker run -d --name watchtower --restart=always -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+
+echo "-------------------------------------------------"
+echo "Complete                                         "
+echo "-------------------------------------------------"
+
